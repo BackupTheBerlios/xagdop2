37e98259ea282f15d8c436327976c475def736cc
nephos <nephos>
2005-11-14 12:45:37 +0000

    Respect des conventions

--- XAGDOP/src/Interface/IProjet.java
+++ XAGDOP/src/Interface/IProjet.java
@@ -24,8 +24,15 @@ public class IProjet extends JFrame{
 	
 	//private CProject cpro = new CProject();
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1560367300092065010L;
 	private static IProjet IP = null;
-	
+	protected TextField TFNp; 
+	protected TextArea TADesc;
+	protected JLabel JlabelNomProjet;
+	protected JPanel JPanelProjetTopContainer;
 
 	private IProjet(){
 		init();
@@ -33,50 +40,50 @@ public class IProjet extends JFrame{
 	
 	
 	private void init(){
-		final TextField np = new TextField(30);
-
-		final TextArea desc= new TextArea(4,30);
+		TFNp = new TextField(30);
+		TADesc= new TextArea(4,30);
+		
 				
 		setTitle("Creer un projet");
 		setSize(300, 400);
-		JPanel projetTopContainer = new JPanel() ;
+		JPanelProjetTopContainer = new JPanel() ;
 		
 			
 		
-		projetTopContainer.setLayout(new FlowLayout(FlowLayout.LEFT)) ;
+		JPanelProjetTopContainer.setLayout(new FlowLayout(FlowLayout.LEFT)) ;
 		
 
 		    
-		projetTopContainer.setBorder(BorderFactory.createEtchedBorder()) ;
+		JPanelProjetTopContainer.setBorder(BorderFactory.createEtchedBorder()) ;
 		//projetTopContainer.setBackground(black) ;
-		projetTopContainer.add(Box.createHorizontalStrut(15)) ;
+		JPanelProjetTopContainer.add(Box.createHorizontalStrut(15)) ;
 
 
-		final JLabel nomProjet = new JLabel("Nom du projet");
-		nomProjet.setToolTipText("<HTML><BODY BGCOLOR=#E3E3E3><FONT COLOR=#0000AB>Nom du projet sur le depot SubVersion</FONT></BODY></HTML>") ;
-		projetTopContainer.add(nomProjet);
-		projetTopContainer.add(np);
-		projetTopContainer.add(new JLabel("Description du projet"));
-		projetTopContainer.add(desc );
+		JlabelNomProjet = new JLabel("Nom du projet");
+		JlabelNomProjet.setToolTipText("<HTML><BODY BGCOLOR=#E3E3E3><FONT COLOR=#0000AB>Nom du projet sur le depot SubVersion</FONT></BODY></HTML>") ;
+		JPanelProjetTopContainer.add(JlabelNomProjet);
+		JPanelProjetTopContainer.add(TFNp);
+		JPanelProjetTopContainer.add(new JLabel("Description du projet"));
+		JPanelProjetTopContainer.add(TADesc);
 		
 		JButton valider = new JButton("valider");
 		valider.addActionListener(new ActionListener()
 				{
 				    public void actionPerformed(ActionEvent e)
 				    {
-					CProject.nouveauProjet(np.getText(),desc.getText());
+					CProject.nouveauProjet(TFNp.getText(),TADesc.getText());
 				    }
 				}) ;
 
-		projetTopContainer.add(valider);
+		JPanelProjetTopContainer.add(valider);
 		
 		//Creation de la fenetre
 		setResizable(true) ;
 
 		setDefaultCloseOperation (WindowConstants.HIDE_ON_CLOSE) ;
 		
-		projetTopContainer.setBorder(BorderFactory.createEtchedBorder()) ;
-		getContentPane().add(projetTopContainer , BorderLayout.CENTER) ;
+		JPanelProjetTopContainer.setBorder(BorderFactory.createEtchedBorder()) ;
+		getContentPane().add(JPanelProjetTopContainer , BorderLayout.CENTER) ;
 		setLocation(new Point(500 , 200)) ;
 
 
