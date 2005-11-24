a6a207c35887124c3e13f532441b0f2e8861b3f9
jeremy <jeremy>
2005-11-24 22:11:56 +0000

    Creation de la classe, en attente du parser

---
+++ XAGDOP/xagdop/Controleur/CTeamManagement.java
@@ -0,0 +1,16 @@
+package xagdop.Controleur;
+
+public class CTeamManagement{
+	
+	public static void Apply(String user,boolean architect, boolean analyst, boolean redacter){
+		//Call to XML Parser
+		
+		//Send file
+		
+	}
+	
+	
+	
+	
+	
+}
