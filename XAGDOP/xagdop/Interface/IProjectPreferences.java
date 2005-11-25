e5990c3c77c2312630d0813d26c67272f988426a
kekile <kekile>
2005-11-25 13:42:26 +0000

    Ajout du bouton annuler et correction de bugs

--- XAGDOP/xagdop/Interface/IProjectPreferences.java
+++ XAGDOP/xagdop/Interface/IProjectPreferences.java
@@ -24,19 +24,24 @@ import xagdop.ressources.Bundle;
 
 public class IProjectPreferences extends JFrame {
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 4722728578653189570L;
 	//private CProject cpro = new CProject();
 	
 	/**
 	 * 
 	 */
-	private static final long serialVersionUID = 1560367300092065010L;
+	
 	private static IProjectPreferences IPP = null;
 	protected TextField TFLP; 
-	protected TextField TFRP;
+	protected JLabel TFRP;
 	protected JLabel JlabelProjectRepositoryPath;
 	protected JLabel JlabelProjectLocalPath;
 	protected JPanel JPanelProjectTopContainer;
-	protected IProjectTree tree = null;	
+	protected IProjectTree tree = new IProjectTree();	
+	
 	private IProjectPreferences(){
 		init();
 	}
@@ -44,7 +49,7 @@ public class IProjectPreferences extends JFrame {
 	
 	private void init(){
 		TFLP = new TextField(30);
-		TFRP = new TextField(30);
+		
 			
 		setTitle(Bundle.getText("iprojectpreference.title"));
 		setSize(261, 235);
@@ -56,7 +61,6 @@ public class IProjectPreferences extends JFrame {
 		JPanelProjectTopContainer.setBorder(BorderFactory.createEtchedBorder()) ;
 	
 		//Initialisation du chemin local
-		JPanelProjectTopContainer.add(Box.createHorizontalStrut(72)) ;
 		JlabelProjectLocalPath = new JLabel(Bundle.getText("iprojectpreference.label.localpath"));
 		JlabelProjectLocalPath.setToolTipText(tree.getSelectedNode().getLocalPath()) ;
 		JPanelProjectTopContainer.add(JlabelProjectLocalPath);
@@ -64,8 +68,7 @@ public class IProjectPreferences extends JFrame {
 		
 		//Initialisation du chemin sur le serveur
 		JlabelProjectRepositoryPath = new JLabel(Bundle.getText("iprojectpreference.label.repositorypath"));
-		JlabelProjectRepositoryPath.setToolTipText(tree.getSelectedNode().getRepositoryPath()) ;
-		JPanelProjectTopContainer.add(Box.createHorizontalStrut(72)) ;
+		TFRP = new JLabel(tree.getSelectedNode().getRepositoryPath()) ;
 		JPanelProjectTopContainer.add(JlabelProjectRepositoryPath);
 		JPanelProjectTopContainer.add(TFRP);
 		
@@ -87,13 +90,13 @@ public class IProjectPreferences extends JFrame {
 				{
 		    public void actionPerformed(ActionEvent e)
 		    {
-		    	   	IPP.setVisible(true);
+		    	IPP.dispose();   	
 		    }
 		}) ;
 		
 		JPanelProjectTopContainer.add(Box.createHorizontalStrut(80)) ;
 		JPanelProjectTopContainer.add(valide);
-		
+		JPanelProjectTopContainer.add(cancel);
 		//Creation de la fenetre
 		setResizable(true) ;
 
@@ -101,6 +104,8 @@ public class IProjectPreferences extends JFrame {
 		
 		JPanelProjectTopContainer.setBorder(BorderFactory.createEtchedBorder()) ;
 		getContentPane().add(JPanelProjectTopContainer , BorderLayout.CENTER) ;
+		
+	
 		setLocation(new Point(500 , 200)) ;
 
 
