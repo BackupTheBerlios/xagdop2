618fe2987fb1f51ddf926097a06cd859286ea5fb
jeremy <jeremy>
2005-11-27 16:43:57 +0000

    ajout des getters+Setters

--- XAGDOP/xagdop/Controleur/CTeamManagement.java
+++ XAGDOP/xagdop/Controleur/CTeamManagement.java
@@ -2,14 +2,26 @@ package xagdop.Controleur;
 
 public class CTeamManagement{
 	
-	public static void Apply(String user,boolean architect, boolean analyst, boolean redacter){
+	String _projectName;
+	public void Apply(String user,boolean architect, boolean analyst, boolean redacter){
 		//Call to XML Parser
 		
 		//Send file
 		
 	}
 	
-	
+	public CTeamManagement(String projectName){
+		this._projectName=projectName;
+		
+	}
+
+	public String get_projectName() {
+		return _projectName;
+	}
+
+	public void set_projectName(String name) {
+		_projectName = name;
+	}
 	
 	
 	
