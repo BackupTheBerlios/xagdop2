c256e2321262b6989c72eaf0a42c83a6b1bfe629
jeremy <jeremy>
2005-12-04 14:13:39 +0000

    *** empty log message ***

--- XAGDOP/xagdop/Controleur/CAffect.java
+++ XAGDOP/xagdop/Controleur/CAffect.java
@@ -1,5 +1,8 @@
 package xagdop.Controleur;
 
+import javax.management.InstanceNotFoundException;
+
+import xagdop.Interface.ITeamManagement;
 import xagdop.Parser.ProjectsParser;
 import xagdop.Parser.UsersParser;
 
@@ -7,49 +10,54 @@ import xagdop.Parser.UsersParser;
 
 public class CAffect {
 	
-	public void affecter(String project,String login, boolean pmanager,boolean archi, boolean redactor, boolean analyst) throws Exception
+	public void affecter(String project,String login, boolean pmanager,boolean archi, boolean redactor, boolean analyst) throws Exception,InstanceNotFoundException
 	{
 		ProjectsParser PP = new ProjectsParser();
 		UsersParser UP = new UsersParser();
 		int id = UP.getId(login);
 		if (id==0)
-			throw new Exception();
+			throw new InstanceNotFoundException();
 		else
 		{
-			String pMana;
-			String arch;
-			String redac;
-			String anal;
+			if (PP.exist(project,login))
+				throw new Exception();
+			else
 		
-			//		Call to XML Parser
+			{
+				String pMana;
+				String arch;
+				String redac;
+				String anal;
+				
+			//			Call to XML Parser
+				
+				
+			//	Apply for Redacteur Value
+				if (pmanager)
+					pMana = "true";
+				else
+					pMana = "false";
+				
+				if (archi)
+					arch = "true";
+				else
+					arch = "false";
+				
+				if (redactor)
+					redac = "true";
+				else
+					redac = "false";
+				
+				if (analyst)
+					anal = "true";
+				else
+					anal = "false";
+				
+				
+				PP.addUser(project, id, pMana, arch, anal,redac);
 
-		
-			//Apply for Redacteur Value
-			if (pmanager)
-				pMana = "true";
-			else
-				pMana = "false";
-			
-			if (archi)
-				arch = "true";
-			else
-				arch = "false";
-			
-			if (redactor)
-				redac = "true";
-			else
-				redac = "false";
-			
-			if (analyst)
-				anal = "true";
-			else
-				anal = "false";
-			
-			
-			//PP.removeUser( project, id);
-			PP.addUser(project, id, pMana, arch, anal,redac);
-		}
-		
+			}
+		}	
 	}
 		
 	
