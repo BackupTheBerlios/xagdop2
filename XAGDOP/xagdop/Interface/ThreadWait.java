b3408423d3c96ad8d2faee572ba6cc0f33bdb822
toine <toine>
2006-01-17 22:06:52 +0000

    Mise en place de la ProgressBar sur la JDialog pour l'attente de la MAJ Pb:-> thread

--- XAGDOP/xagdop/Interface/ThreadWait.java
+++ XAGDOP/xagdop/Interface/ThreadWait.java
@@ -1,9 +1,16 @@
 package xagdop.Interface;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+
+import javax.swing.BorderFactory;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JProgressBar; 
+
+import sun.awt.windows.ThemeReader;
 
 public class ThreadWait extends Thread {
 
@@ -11,34 +18,35 @@ public class ThreadWait extends Thread {
 	int i=0;
 	JLabel label = new JLabel("i");
 	JPanel panel = new JPanel();
+	JProgressBar progressBar = new JProgressBar();
 	private boolean Stop;
 	
-	public ThreadWait(JFrame jf){
+	public ThreadWait(JFrame jf){	
 		JD = new JDialog(jf,"Encours",true);
-		//JD.setTitle("En cours");
-		JD.setSize(536, 500);
-		panel.add(label);
-		JD.getContentPane().add(panel);
-		
-		
-		
-		
 	}
 	
 	public void run() {
-		JD.setVisible(true);
-		i=0;
-		Stop = false;
-		while (!Stop)
-			{
-			i++;
-			label.setText(new Integer(i).toString());
-			label.validate();
-			}
-			
+		
+	
+		// Traitement du thread
+		
+		System.out.println("TOTO");
+		this.init();
+		//i=0;
+		//Stop = false;
+		//while (!Stop)
+			//{
+			//i++;
+			//label.setText(new Integer(i).toString());
+		  	//label.validate();
+		  	
+			//}
+		
+		
 		System.out.println("balbla");
-		JD.setVisible(false);
-		Stop = false;
+		//JD.setVisible(false);
+		//Stop = false;
+		
     }
 
 	public void setStop(boolean stop) {
@@ -54,5 +62,27 @@ public class ThreadWait extends Thread {
 	{
 		JD.setVisible(true);	
 	}
+	
+	public void init()
+	{
+		
+		JD.setSize(300, 300);
+		panel.add(label);
+		//panel.add(progressBar);
+		panel.add(label);
+		
+		//JD.getContentPane().add(progressBar);
+		JD.getContentPane().add(label);
+		
+		progressBar.setIndeterminate(true);
+		progressBar.setStringPainted(false);
+		progressBar.setPreferredSize(new Dimension(250, 20));
+		JPanel progressPanel = new JPanel();
+		progressPanel.add(progressBar);
+		progressPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+		JD.add(progressPanel, BorderLayout.CENTER);
+		JD.pack();
+	}
+	
 	
 }
