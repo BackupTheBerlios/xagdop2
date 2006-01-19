877372170f4961a500e5154c844f4f064077d2fa
kekile <kekile>
2006-01-19 10:40:05 +0000

    fonction de verification du mot de passe login

--- XAGDOP/xagdop/Controleur/CUser.java
+++ XAGDOP/xagdop/Controleur/CUser.java
@@ -1,25 +1,32 @@
 package xagdop.Controleur;
+
+
+import javax.swing.JOptionPane;
+import xagdop.Interface.IPreferences;
+import xagdop.Interface.XAGDOP;
+import xagdop.Model.Users;
+import xagdop.Parser.UsersParser;
+
 public class CUser {
-	/*
-	 * 
-	 * Attributs de la classe
-	 * 
-	 * 
-	 */
 	
-	/*
-	 * Constructeur
-	 */
 	public CUser(){
-		
 	}
 	
-	/*
-	 * Methode de la classe
-	 */
-	
-	
-	
-	
-	
+	public boolean verifUser(String login, String passwd){
+		UsersParser UParser = new UsersParser();
+		Users user = UParser.getUser(login,passwd);
+    	if (user != null){
+    		IPreferences.setDefaultPath(IPreferences.getDefaultPath()+user.getLogin()+"/");
+    		XAGDOP.getInstance().setUser(user);
+    		XAGDOP.getInstance().setVisible(true);
+    		XAGDOP.getInstance().refreshButton();
+    		return true;
+    	}
+    	else{
+    		JOptionPane.showMessageDialog(null ,"Login incorrect", "Validation" , 1) ;
+    		return false;
+    	}
+		
+	}
 }
+	
