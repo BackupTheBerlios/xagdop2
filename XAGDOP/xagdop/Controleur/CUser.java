ae81aaaf126e2f1b48b38d8d55e5ac15fdb83a33
kekile <kekile>
2006-01-19 16:05:44 +0000

    Ajout de la fonction permettant de créer un nouvel utilisateur

--- XAGDOP/xagdop/Controleur/CUser.java
+++ XAGDOP/xagdop/Controleur/CUser.java
@@ -1,6 +1,7 @@
 package xagdop.Controleur;
 
 
+import javax.management.InstanceNotFoundException;
 import javax.swing.JOptionPane;
 import xagdop.Interface.IPreferences;
 import xagdop.Interface.XAGDOP;
@@ -27,6 +28,28 @@ public class CUser {
     		return false;
     	}
 		
+	}
+	
+	public boolean creerUser(String login, String passwd, String passwdconf) throws Exception,InstanceNotFoundException
+	{
+		UsersParser UP = new UsersParser();
+		int idUser = 2;
+		if (passwd.equals(passwdconf)){
+    		
+			if (!UP.isUser(UP.getId(login))){
+				UP.addUser(idUser,login,passwd);
+				JOptionPane.showMessageDialog(null ,"Utilisateur creer", "Validation" , 1) ;
+	    		return true;
+			}
+			else{
+				JOptionPane.showMessageDialog(null ,"Login deja utilise", "Validation" , 1) ;
+				return false;
+			}
+		}
+		else{
+    		JOptionPane.showMessageDialog(null ,"Les deux mots de passe ne sont pas identiques", "Validation" , 1) ;
+    		return false;
+		}
 	}
 }
 	
