beadb38ad7e74bd66e63aca2f97e2fb2c6143489
toine <toine>
2006-01-24 11:52:38 +0000

    Fichier IJTeamManagementTableModel pour la Jtable Affectation des ?quipes

--- XAGDOP/xagdop/Interface/IJTeamManagementTableModel.java
+++ XAGDOP/xagdop/Interface/IJTeamManagementTableModel.java
@@ -3,10 +3,13 @@ package xagdop.Interface;
 import java.util.ArrayList;
 import java.util.Iterator;
 
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.table.AbstractTableModel;
 
 import xagdop.Parser.ProjectsParser;
 import xagdop.Model.Projects;
+import xagdop.Model.Users;
 
 
 public class IJTeamManagementTableModel extends AbstractTableModel {
@@ -16,42 +19,23 @@ public class IJTeamManagementTableModel extends AbstractTableModel {
 	 */
 	private static final long serialVersionUID = 1L;
 	
-	private String nomColonne[] = {"Users","Redacteur","Analyste","PManager","Desaffecter"};
+	private String nomColonne[] = {"Users","Redacteur","Analyste","PManager","Architecte","Desaffecter"};
 	private Object[][] rowData;
 	private ArrayList users;
 	private ProjectsParser projetParser;
 	private Projects projet;
+	private String nP;
 	
 	public IJTeamManagementTableModel(ArrayList usersParser, String nomProjet) {
-	
+	    this.nP = nomProjet;
 		this.users = usersParser;
 		projetParser = new ProjectsParser(); 
 		
 		nomProjet = this.projet.getProjectName();
 		projet = projetParser.getAllUsers(nomProjet);
 		
-		//ArrayList usersID = projet.getUsersId();
-		
-		
-		this.rowData = new Object[users.size()][5];
-		Iterator it = users.iterator();
-	//	int i = 0;
-		while (it.hasNext())
-		{
+		this.init(users);
 			
-		//	rowData[i][0] =	username.getAttribute(usersID,"login");	// users
-		//	rowData[i][1] = 							// Redacteur
-		//	rowData[i][2] = 							// Analyste
-		//	rowData[i][3] =  							// Pmanager
-		//	rowData[i][4] =								// Bouton desaffecter  
-			
-		}
-		
-		
-		
-		
-		
-		
 		
 	}
 	
@@ -78,5 +62,23 @@ public class IJTeamManagementTableModel extends AbstractTableModel {
 		return this.nomColonne[colonne];
 	}
 	
+	public void init(ArrayList ff) {
+		this.users = ff;
+		this.rowData = new Object[users.size()][5];
+		Iterator it = users.iterator();
+		int i = 0;
+		while (it.hasNext())
+		{
+			Object o = it.next();
+			rowData[i][0] =	((Users)o).getLogin();	
+			rowData[i][1] = new Boolean(((Users)o).isPManager(this.nP));						
+			rowData[i][2] = new Boolean(((Users)o).isAnalyst(this.nP));							
+			rowData[i][3] = new Boolean(((Users)o).isArchitect(this.nP)); 							
+			rowData[i][4] = new Boolean(((Users)o).isRedactor(this.nP));
+			rowData[i][5] = new JButton(new ImageIcon(XAGDOP.class.getResource("/xagdop/ressources/Icon/supprimer.gif")));
+			i++;	
+		}
+		
+	}
 	
 }
