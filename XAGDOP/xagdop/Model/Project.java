f77688cf26dbe51c492f28f8cf50c026bacabda8
titeclaire <titeclaire>
2006-01-27 12:48:24 +0000

    Changement de nom : Projects -> Project Users -> User

---
+++ XAGDOP/xagdop/Model/Project.java
@@ -0,0 +1,90 @@
+package xagdop.Model;
+
+import java.util.ArrayList;
+
+public class Project {
+
+	private String projectName;
+	
+	//La liste contient pour chaque id de User, la liste des droits
+	//manager, architecte, analyste, r??dacteur
+	private ArrayList rights;
+	//La liste des logins des utilisateurs travaillant sur le projet
+	private ArrayList usersLogin;
+	
+	public Project(String name, ArrayList rights, ArrayList logins)
+	{
+		this.projectName = name;
+		this.rights = rights;
+		this.usersLogin = logins;
+	}
+	
+	public String getProjectName() {
+		return projectName;
+	}
+	
+	public void setProjectName(String projectName) {
+		this.projectName = projectName;
+	}
+
+	public ArrayList getUsersLogin() {
+		return usersLogin;
+	}
+
+	public void setUsersLogin(ArrayList usersLogins) {
+		this.usersLogin = usersLogins;
+	}
+	
+	public boolean isManager(String login)
+	{
+		boolean res = false;
+		int index = usersLogin.indexOf(login);
+		ArrayList droits = (ArrayList)rights.get(index);
+		
+
+		if(((Boolean)droits.get(0)).booleanValue()) {
+			res = true;
+		}
+		
+		return res;
+	}
+	
+	public boolean isArchitecte(String login)
+	{
+		boolean res = false;
+		int index = usersLogin.indexOf(login);
+		ArrayList droits = (ArrayList)usersLogin.get(index);
+		
+		if(((Boolean)droits.get(1)).booleanValue()) {
+			res = true;
+		}
+		
+		return res;
+	}
+	
+	public boolean isAnalyst(String login)
+	{
+		boolean res = false;
+		int index = usersLogin.indexOf(login);
+		ArrayList droits = (ArrayList)usersLogin.get(index);
+		
+		if(((Boolean)droits.get(2)).booleanValue()) {
+			res = true;
+		}
+		
+		return res;
+	}
+	
+	public boolean isRedacteur(String login)
+	{
+		boolean res = false;
+		int index = usersLogin.indexOf(login);
+		ArrayList droits = (ArrayList)usersLogin.get(index);
+		
+		if(((Boolean)droits.get(3)).booleanValue()) {
+			res = true;
+		}
+		
+		return res;
+	}
+}
---
+++ XAGDOP/xagdop/Model/User.java
@@ -0,0 +1,78 @@
+package xagdop.Model;
+
+import xagdop.Parser.ProjectsParser;
+
+public class User {
+	private String login;
+	private String passwd;
+	private boolean admin;
+	private boolean pcreator;
+	
+	public User(String log, String pass, boolean adm, boolean pcreat)
+	{
+		login = log;
+		passwd = pass;
+		admin = adm;
+		pcreator = pcreat;
+	}
+	
+	// Utilise pour le debug
+	public String toString() {
+		StringBuffer sb;
+		sb = new StringBuffer("\nlogin : " + login);
+		sb.append("\npass : " + passwd);
+		if (admin) sb.append("admin");
+		if (pcreator) sb.append("pcreator");
+		return sb.toString();
+	}
+	
+	public boolean isAdmin() {
+		return admin;
+	}
+	public void setAdmin(boolean admin) {
+		this.admin = admin;
+	}
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	public String getPasswd() {
+		return passwd;
+	}
+	public void setPasswd(String passwd) {
+		this.passwd = passwd;
+	}
+	public boolean isPcreator() {
+		return pcreator;
+	}
+	public void setPmanager(boolean pcreat) {
+		this.pcreator = pcreat;
+	}
+	
+	public boolean isArchitect(String pName)
+	{
+		ProjectsParser projet = new ProjectsParser();
+		return ((Boolean)projet.getAttribute(pName, ProjectsParser.ATTR_ARCHI, this.login)).booleanValue();
+	}
+
+	public boolean isRedactor(String pName)
+	{
+		ProjectsParser projet = new ProjectsParser();
+		return ((Boolean)projet.getAttribute(pName, ProjectsParser.ATTR_REDACTEUR, this.login)).booleanValue();
+	}
+
+	public boolean isAnalyst(String pName)
+	{
+		ProjectsParser projet = new ProjectsParser();
+		return ((Boolean)projet.getAttribute(pName, ProjectsParser.ATTR_ANALYST, this.login)).booleanValue();
+	}
+
+	public boolean isPManager(String pName)
+	{
+		ProjectsParser projet = new ProjectsParser();
+		return ((Boolean)projet.getAttribute(pName, ProjectsParser.ATTR_MANAGER, this.login)).booleanValue();
+	}
+
+}
