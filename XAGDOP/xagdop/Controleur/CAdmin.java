fde0c8fb3c5ad9a1e8acbd77a11d88a536172b33
blackmilk <blackmilk>
2006-01-27 14:44:19 +0000

    Modif pmanager pour pcreator

--- XAGDOP/xagdop/Controleur/CAdmin.java
+++ XAGDOP/xagdop/Controleur/CAdmin.java
@@ -7,7 +7,7 @@ import xagdop.Parser.UsersParser;
 public class CAdmin {
 
 public void Apply(UsersParser UP,String user,boolean admin, boolean pmanager){
-		UP.setAttribute(user,"pmanager",pmanager);
+		UP.setAttribute(user,"pcreator",pmanager);
 		UP.setAttribute(user,"admin",admin);
 	}
 
