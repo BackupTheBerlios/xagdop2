98b3c70e92f502645cf2dfaec5b9e5e7ad5a5230
mikamikaze <mikamikaze>
2006-01-31 15:41:53 +0000

    ajout ligne commentee pour connection serveur svn local pour mika

--- XAGDOP/xagdop/Svn/SvnConnect.java
+++ XAGDOP/xagdop/Svn/SvnConnect.java
@@ -12,6 +12,7 @@ import org.tmatesoft.svn.core.wc.SVNWCUtil;
 
 public class SvnConnect {
 	protected String _url = "svn://marine.edu.ups-tlse.fr/users/iupisi/m1isb4/svn/XAGDOP/";//"svn://marine.edu.ups-tlse.fr/users/iupisi/m1isb4/svn/repositoryAubry";
+	//protected String _url = "svn://127.0.0.1/svn/XAGDOP/"; //debug Mika
 	protected String _name = "XAGDOP";
 	protected String _password = "blabla";
 	protected SVNRepository repository = null;
