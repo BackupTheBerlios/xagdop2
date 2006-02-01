3fab6722ff5d54b4573e1e609d03d8b7f28ce34e
blackmilk <blackmilk>
2006-02-01 13:51:58 +0000

    Modification de l'interface graphique

--- XAGDOP/xagdop/Interface/ICommit.java
+++ XAGDOP/xagdop/Interface/ICommit.java
@@ -5,13 +5,14 @@ import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
-
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import javax.swing.WindowConstants;
+import javax.swing.JScrollPane;
 
 
 import xagdop.Controleur.CCommit;
@@ -39,28 +40,24 @@ public class ICommit extends JDialog {
 	private void init(){
 		getContentPane().setLayout(new GridBagLayout());
 		JTAComment = new JTextArea(10,20);
+		JTAComment.setLineWrap(true);
 
 		JlabelComment = new JLabel(Bundle.getText("icommit.label.comment"));
 		
 		// Initialisation de la popup
 		setTitle(Bundle.getText("icommit.title"));
-		setSize(500,300);
+		setSize(330,250);
 	
 		setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
 		JPanelProjectTopContainer.setLayout(new GridBagLayout());
 		JPanelProjectTopContainer.setMinimumSize(new Dimension(296, 130));
-        
-		//Affichage du label
-        gridBagConstraints.gridx = 0;
-        gridBagConstraints.gridy = 0;
-        gridBagConstraints.gridwidth = 2;
-        JPanelProjectTopContainer.add(JlabelComment, gridBagConstraints);
-       
-        //Affichage des commentaires
-        gridBagConstraints.gridx = 0;
-        gridBagConstraints.gridy = 1;
-        gridBagConstraints.gridwidth = 2;
-        JPanelProjectTopContainer.add(JTAComment, gridBagConstraints);
+
+        //Affichage du champ de saisie des commentaires
+        //gridBagConstraints.gridx = 0;
+        //gridBagConstraints.gridy = 1;
+        //gridBagConstraints.gridwidth = 2;
+        this.donnerContrainte(gridBagConstraints,0,2,2,1,100,100,GridBagConstraints.NONE);
+        JPanelProjectTopContainer.add(new JScrollPane(JTAComment), gridBagConstraints);
 
         // creation des boutons de validation et d'annulation
         valide = new JButton(Bundle.getText("icommit.button.ok"));
@@ -87,23 +84,76 @@ public class ICommit extends JDialog {
 		}) ;
 		
 		
-        gridBagConstraints.gridx = 0;
-        gridBagConstraints.gridy = 2;
-        gridBagConstraints.gridwidth = 1;
+        //gridBagConstraints.gridx = 0;
+        //gridBagConstraints.gridy = 2;
+        //gridBagConstraints.gridwidth = 1;
+        this.donnerContrainte(gridBagConstraints,0,3,1,1,50,50,GridBagConstraints.NONE);
         JPanelProjectTopContainer.add(valide, gridBagConstraints);
 
 
-        gridBagConstraints.gridx = 1;
-        gridBagConstraints.gridy = 2;
-        gridBagConstraints.gridwidth = 1;
+        //gridBagConstraints.gridx = 1;
+       // gridBagConstraints.gridy = 2;
+        //gridBagConstraints.gridwidth = 1;
+        this.donnerContrainte(gridBagConstraints,1,3,1,1,50,50,GridBagConstraints.NONE);
         JPanelProjectTopContainer.add(cancel, gridBagConstraints);
 
+//      Affichage du label
+        //gridBagConstraints.gridx = 0;
+        //gridBagConstraints.gridy = 0;
+        //gridBagConstraints.gridwidth = 2;
+        this.donnerContrainte(gridBagConstraints,0,0,2,1,100,100,GridBagConstraints.NONE);
+        gridBagConstraints.anchor = GridBagConstraints.CENTER;
+        JPanelProjectTopContainer.add(JlabelComment, gridBagConstraints);
+        
         getContentPane().add(JPanelProjectTopContainer, new GridBagConstraints());
 
-        pack();
+        //pack();
         setLocation(200,200) ;
         setModal(true);
         setVisible(true);
 	
 	}
+	
+	/** Panel de message de fin de transaction
+     * @param gbc <CODE>GridBagConstraints</CODE> represente la contrainte qui va prendre les
+     * valeurs specifiees
+     * @param gx <CODE>int</CODE> represente la colonne dans laquelle l'?l?ment va etre place
+     * @param gy <CODE>int</CODE> represente la ligne dans laquelle l'?l?ment va etre place
+     * @param gw <CODE>int</CODE> represente le nombre de colonnes sur lesquelles l'?l?ment va etre place
+     * @param gh <CODE>int</CODE> represente le nombre de lignes sur lesquelles l'?l?ment va etre place
+     * @param wx <CODE>int</CODE> poucentage de place utilise dans sa colonne
+     * @param wy <CODE>int</CODE> poucentage de place utilise dans sa ligne
+     */
+	public void donnerContrainte(GridBagConstraints gbc, int gx, int gy, int gw, int gh, int wx, int wy)
+    {
+		gbc.gridx=gx;
+		gbc.gridy=gy;
+		gbc.gridwidth=gw;
+		gbc.gridheight=gh;
+		gbc.weightx=wx;
+		gbc.weighty=wy;
+		gbc.fill=GridBagConstraints.NONE;
+    }
+    
+    /** Panel de message de fin de transaction
+     * @param gbc <CODE>GridBagConstraints</CODE> represente la contrainte qui va prendre les
+     * valeurs specifiees
+     * @param gx <CODE>int</CODE> represente la colonne dans laquelle l'?l?ment va etre place
+     * @param gy <CODE>int</CODE> represente la ligne dans laquelle l'?l?ment va etre place
+     * @param gw <CODE>int</CODE> represente le nombre de colonnes sur lesquelles l'?l?ment va etre place
+     * @param gh <CODE>int</CODE> represente le nombre de lignes sur lesquelles l'?l?ment va etre place
+     * @param wx <CODE>int</CODE> poucentage de place utilise dans sa colonne
+     * @param wy <CODE>int</CODE> poucentage de place utilise dans sa ligne
+     * @param constraint <CODE>int</CODE> contrainte de redimensionnement
+     */    
+    public void donnerContrainte(GridBagConstraints gbc, int gx, int gy, int gw, int gh, int wx, int wy, int constraint)
+    {
+    	gbc.gridx=gx;
+    	gbc.gridy=gy;
+    	gbc.gridwidth=gw;
+    	gbc.gridheight=gh;
+    	gbc.weightx=wx;
+    	gbc.weighty=wy;
+    	gbc.fill=constraint;
+    }
 }
