0011753a97afdc63b84c09b19cea2ad188a8325d
mikamikaze <mikamikaze>
2006-02-02 19:35:12 +0000

    maj diverses

---
+++ XAGDOP/xagdop/Model/Preferencies.java
@@ -0,0 +1,75 @@
+package xagdop.Model;
+
+import java.awt.Component;
+import java.util.ArrayList;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+public class Preferencies {
+	String server="";
+	String lookNFeel="";
+	String langue="";
+	
+	
+	public Preferencies(String server, String lookNFeelName,String langue){
+		this.server=server;
+		this.lookNFeel=lookNFeelName;	
+		this.langue=langue;
+	}
+	
+	
+	/**
+	 * Permet d'obtenir la liste des noms des classes de LookNFeel disponibles sur la machine.(pour fixer)
+	 * @return ArrayList de String contenant le nom du LNF
+	 */
+	public static ArrayList getAvaillableLNFName(){
+		ArrayList availlLNF = new ArrayList();
+		UIManager.LookAndFeelInfo[] info = UIManager.getInstalledLookAndFeels();
+		for(int i=0; i < info.length; i++){
+			System.out.println(info[i].getName());
+			availlLNF.add(info[i].getName());
+		}
+		return availlLNF;
+	}	
+	
+	
+	/**
+	 * Permet d'obtenir le nom de la classe LNF correspondant a un nom de LNF
+	 * @param name Nom du LNF
+	 * @return Nom de la classe du LNF
+	 */
+	public static String getClassName (String name)
+	{
+		UIManager.LookAndFeelInfo[] info = UIManager.getInstalledLookAndFeels();
+		int i = 0;
+		while (i < info.length && !(info[i].getName().equals(name))){
+			i++;
+		}
+		if (i < info.length){
+			return info[i].getClassName();
+		}
+		else{
+			return "";	
+		}
+	}
+			
+	
+	/**
+	 * Applique un LNF a un composant graphique
+	 * @param className nom de la classe du LNF
+	 * @param comp Composant a mettre a jour
+	 */
+	public static void changeLookandFeel (String className, Component comp)
+	{
+		try {
+		    UIManager.setLookAndFeel(className);
+	  		SwingUtilities.updateComponentTreeUI(comp);
+		}	
+		catch (InstantiationException ex) {ex.printStackTrace();}
+		catch (ClassNotFoundException ex) {ex.printStackTrace();}
+		catch (UnsupportedLookAndFeelException ex) {ex.printStackTrace();}
+		catch (IllegalAccessException ex) { ex.printStackTrace(); }
+	}
+	
+}
--- XAGDOP/xagdop/Parser/PreferenciesParser.java
+++ XAGDOP/xagdop/Parser/PreferenciesParser.java
@@ -1,29 +1,94 @@
 package xagdop.Parser;
 
 import java.io.File;
+import java.util.ArrayList;
 
-import xagdop.Svn.SvnUpdate;
+import javax.swing.UIManager;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
+import org.w3c.dom.Element;
+import xagdop.Model.Preferencies;
 
 public class PreferenciesParser extends Parser{
 	private File preferenciesXML;
-	private static PreferenciesParser PPInstance = null;
+	private static PreferenciesParser PrfPInstance = null;
+	private static String SERV_URL = "url";
+	private static String LNF_NAME = "lnf";
+	private static String LANG_NAME = "langue";
+	
 	
 	private PreferenciesParser()
 	{
 		try {
-			preferenciesXML = new File("xagdop/ressources/XML/preferencies.xml"); //debug
+			preferenciesXML = new File("xagdop/ressources/XML/preferencies.xml");
 			loadTreeInMemory(preferenciesXML);
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 	
 	
 	public static PreferenciesParser getInstance() {
-		if (PPInstance == null)
-			PPInstance = new PreferenciesParser();
-		return PPInstance;
+		if (PrfPInstance == null)
+			PrfPInstance = new PreferenciesParser();
+		return PrfPInstance;
 	}
+	
+	/**
+	 * Construit un objet Preferencies a partir du fichier XML
+	 * @return L'objet Preferencies
+	 */
+	public Preferencies buildPreferencies(){
+		String server="";
+		String lnf="";
+		String lang="";
+		Element elem=null;
+		XPath xpath = XPathFactory.newInstance().newXPath();
+		
+		//recuperation de l'URL du serveur
+		String expression = "//server";
+		try {
+			elem = (Element)xpath.evaluate(expression, this.doc, XPathConstants.NODE);
+		}
+		catch (XPathExpressionException e) {
+			e.printStackTrace();
+		}
+		if ( elem != null ) {
+			server= elem.getAttribute(PreferenciesParser.SERV_URL);
+		}
+		
+		//recuperation du LookNFeel
+		expression = "//lookNFeel";
+		try {
+			elem = (Element)xpath.evaluate(expression, this.doc, XPathConstants.NODE);
+		}
+		catch (XPathExpressionException e) {
+			e.printStackTrace();
+		}
+		if ( elem != null ) {
+			lnf = elem.getAttribute(PreferenciesParser.LNF_NAME);
+		}
+		
+		//recuperation de la langue
+		expression = "//langue";
+		try {
+			elem = (Element)xpath.evaluate(expression, this.doc, XPathConstants.NODE);
+		}
+		catch (XPathExpressionException e) {
+			e.printStackTrace();
+		}
+		if ( elem != null ) {
+			lang= elem.getAttribute(PreferenciesParser.LANG_NAME);
+		}
+		
+
+		Preferencies pref = new Preferencies(server,lnf,lang);
+		return pref;
+	}
+	
+
+	
 	
 }
