0e215c67d3fa770141fdcc77387f5b660cb43e3e
nephos <nephos>
2006-02-02 13:09:39 +0000

    Ajout de la fonction permettant la recup?ration de toutes les d?pendances

--- XAGDOP/xagdop/Controleur/CDependencies.java
+++ XAGDOP/xagdop/Controleur/CDependencies.java
@@ -1,5 +1,11 @@
 package xagdop.Controleur;
 
+import java.io.File;
+import java.io.FilenameFilter;
+import java.util.HashMap;
+
+import xagdop.Interface.IPreferences;
+
 
 
 public class CDependencies {
@@ -14,7 +20,7 @@ public class CDependencies {
 	/*
 	 * Constructeur
 	 */
-	public CDependencies(String apes){
+	public CDependencies(){
 	
 	}
 	
@@ -22,6 +28,35 @@ public class CDependencies {
 	 * Methode de la classe
 	 */
 	
+	/**
+	 * @return
+	 */
+	public HashMap getDependenciesFiles(){
+		HashMap dependencies = new HashMap();
+		
+		File projectsDirectory = new File(IPreferences.getDefaultPath());
+		File[] projects = projectsDirectory.listFiles(new FilenameFilter() {
+			
+			public boolean accept(File dir, String name) {
+				File directory = new File(dir+File.separator+name);
+				if(directory.isDirectory()&&!directory.isHidden())
+					return true;
+				return false;
+			}
+		});
+		
+		File dependencyFile;
+		if(projects!=null){
+			for(int i= 0; i < projects.length;i++){
+				dependencyFile = new File(projects[i]+File.separator+"dependencies.xml");
+				if(dependencyFile.exists())
+					dependencies.put(projects[i].getName(),dependencyFile);
+				
+			}
+		}
+		
+		return dependencies;
+	}
 	
 	
 	
