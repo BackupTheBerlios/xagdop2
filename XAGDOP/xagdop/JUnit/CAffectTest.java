8b3df7bddd2f313ba63db6fe118ee902a90cfca1
elween <elween>
2006-02-03 07:48:07 +0000

    Modification du Junit pour la methode apply de CAffect

--- XAGDOP/xagdop/JUnit/CAffectTest.java
+++ XAGDOP/xagdop/JUnit/CAffectTest.java
@@ -6,6 +6,8 @@ import javax.management.InstanceNotFoundException;
 
 import xagdop.Controleur.CAffect;
 import xagdop.Model.*;
+import xagdop.Parser.ProjectsParser;
+import xagdop.Parser.UsersParser;
 
 import junit.framework.TestCase;
 
@@ -16,16 +18,24 @@ public class CAffectTest extends TestCase {
 	 */
 	public void testAffecter() {
 		CAffect CA = new CAffect();
-		ArrayList list= new ArrayList();
-		ArrayList id =new ArrayList();
 		
-		Project p = new Project("blabla",list,id);
+		//Creer un nouveau utilisateur
 		User us = new User("toto","totopass",true,false);
+		UsersParser usrp = UsersParser.getInstance();
+		usrp.addUser(us);
 		
+		//Creer un nouveau projet
+		ProjectsParser pp = ProjectsParser.getInstance();
+		pp.addProject("projTest",us,"projDescr");
+
 		try {
-			CA.affecter(p.getProjectName(),us.getLogin(),true,false,true,false);
-			assertTrue(p.isRedactor(us.getLogin())==true);
-			assertFalse(p.isManager(us.getLogin())==false);
+			//Tests
+			CA.affecter("projTest",us.getLogin(),true,false,true,false);
+			assertTrue(us.isRedactor("projTest"));
+			assertTrue(!us.isAnalyst("projTest"));
+			assertTrue(us.isPManager("projTest"));
+			assertTrue(!us.isArchitect("projTest"));
+			
 		} catch (InstanceNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -34,7 +44,11 @@ public class CAffectTest extends TestCase {
 			e.printStackTrace();
 		}
 		
+		//Supprimer l'utilisateur
+		usrp.removeUser("toto");
 		
+		//Supprimer le projet
+		pp.removeProject("projTest");
 		   
 	}
 
