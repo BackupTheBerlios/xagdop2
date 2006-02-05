6244f47d50d77567e8f9a316163a47fd4221a064
titeclaire <titeclaire>
2006-02-05 12:13:09 +0000

    Mise à jour pout que la classe hérite de Parser

--- XAGDOP/xagdop/Parser/POGParser.java
+++ XAGDOP/xagdop/Parser/POGParser.java
@@ -2,26 +2,15 @@ package xagdop.Parser;
 
 import java.io.File;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerConfigurationException;
-import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
 import javax.xml.xpath.XPath;
 import javax.xml.xpath.XPathConstants;
 import javax.xml.xpath.XPathExpressionException;
 import javax.xml.xpath.XPathFactory;
 
-import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
-public class POGParser {
-	private DocumentBuilderFactory dbf;
-	private DocumentBuilder db;
-	private Document doc;
+public class POGParser extends Parser{
+
 	private File pogFile;
 	
 	public POGParser(File fichier) {
@@ -34,12 +23,6 @@ public class POGParser {
 		}
 	}
 	
-	private void loadTreeInMemory(File fichier) throws Exception {
-		this.dbf = DocumentBuilderFactory.newInstance();
-		this.dbf.setValidating(false);
-		this.db = dbf.newDocumentBuilder();
-		this.doc = db.parse(fichier);
-	}
 	
 	public void refresh()
 	{
@@ -91,36 +74,13 @@ public class POGParser {
 		if(path!=null)
 		{
 			path.setTextContent(pathRelative);
-			saveDocument();
+			saveDocument(pogFile);
 		}
 		else {
 			System.out.println("Pb setApesPathToRelative!"); 
 		}	
 	}
 	
-	public void saveDocument()
-	{	
-		TransformerFactory tFactory = TransformerFactory.newInstance();
-		Transformer transformer = null;
-		try {
-			transformer = tFactory.newTransformer();
-			} catch (TransformerConfigurationException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} 
-		try {
-			transformer.transform(new DOMSource(doc), new StreamResult(pogFile));
-			//SvnCommit svnc = new SvnCommit();
-			//svnc.sendFile(dependenciesXML,"");
-			loadTreeInMemory(pogFile);
-		} catch (TransformerException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	
-	}
+
 	
 }
