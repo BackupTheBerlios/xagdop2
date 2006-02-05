290b97157b73ec3b17ce2b8333215a248ad268c6
jeremy <jeremy>
2006-02-05 13:17:43 +0000

    Décomposition en sous-fonctions + Commentaires ajoutés + Ajout des prises en compte des fichier Iepp !!!
    Beaucoup de boulot quoi !

--- XAGDOP/xagdop/Controleur/CCommit.java
+++ XAGDOP/xagdop/Controleur/CCommit.java
@@ -10,6 +10,7 @@ import org.tmatesoft.svn.core.SVNException;
 import xagdop.Interface.IProjectTree;
 import xagdop.Interface.XAGDOP;
 import xagdop.Parser.DependenciesParser;
+import xagdop.Parser.IeppNitParser;
 import xagdop.Parser.POGParser;
 import xagdop.Svn.SvnCommit;
 import xagdop.Svn.SvnHistory;
@@ -23,13 +24,14 @@ public class CCommit{
 	public CCommit(CTreeNode currentNode){
 		recCommit(currentNode,".apes");
 		recCommit(currentNode,".pog");
+		recCommit(currentNode,".iepp");
 	}
 
 	
 	
 	//Permet d'envoyer le fichier contenu dans le Noeud
 	public void beforeCommit(CTreeNode node){
-		DependenciesParser DP = DependenciesParser.getInstance();
+	
 		//Recuperation du nom de fichier a envoyer
 		String nameOfFile = node.getName();
 		
@@ -49,67 +51,11 @@ public class CCommit{
 			//-----------------------------------------
 			if (nameOfFile.endsWith(".apes"))
 			{
-
-				//Si le fichier est tout neuf
-				if (!SvnHistory.isUnderVersion(toCommit))
-				{
-					//Pas d'entrainement de verification de fichier pog, car fichier tout nouveau
-
-					//Rajout dans le fichier de d?pendances le fichier apes que l'on veut envoyer
-					
-					DP.addApes(pathToRoot);
-					
-					
-					
-										
-				}
-//				Si le fichier est ancien
-				else if (node.isModified())
-				{
-
-					//On vient de modifier le fichier apes
-					//Il faut donc dire de changer les pog dependants
-					
-					
-					//On recupere la liste de tous les pogs dependant de l'apes
-					
-					ArrayList allPog = DP.getPogFromApes(pathToRoot);
-					
-					//Initialisation du parcours
-					int i = 0;
-
-					
-
-					//On parcours la liste
-					for (i=0;i<allPog.size();i++)
-					{
-						//On indique qu'il faut mettre a jour tous les fichiers
-						//Qui sont d?pendants du fichier apes que l'on veux envoyer
-						//A condition qu'il ne soit pas deja a modifier
-						if (!DP.isToUpdate((String)allPog.get(i)))
-						{
-							//Ajouter dans la liste
-							DP.addToUpdate((String)allPog.get(i));
-						}
-					}
-					
-
-				}
-				
-					//Cas d'erreur
-				else
-				{
-				
-				}
 				
 				
-				
-				
+				sendApesFile(toCommit,node,pathToRoot);
 				
 			}
-			
-			
-			
 			
 			//-----------------------------------------
 			//Si le fichier est un pog -->
@@ -117,66 +63,21 @@ public class CCommit{
 			else if (nameOfFile.endsWith(".pog"))
 			{
 				
-				//Si le fichier est tout neuf
-				if (!SvnHistory.isUnderVersion(toCommit))
-				{
+				sendPogFile(toCommit,node,pathToRoot);
 				
-					//Modifier l'interieur du fichier pog en
-					//mettant en relatif le chemin du fichier apes dependant
-					
-					//Ouverture du parser du fichier POG Correspondant
-					POGParser POGP = new POGParser(toCommit);	
-					//Recuperation du chemin absolue du fichier apes d?pendant
-					String pathDependantApesFile = POGP.getPathApesFile();
-					
-					if (!pathDependantApesFile.equals(""))
-					{
-						//Recuperation du path absolue de la racine des projets
-						String pathGlobal = ((CTreeNode)((IProjectTree)XAGDOP.getInstance().getTree()).getModel().getRoot()).getLocalPath();
-						//Recuperation du pathToRoot du fichier Apes en 
-						//Enlevant les premiers caracteres correspondant au debut du chemin absolue
-						String pathSemiGlobal = pathDependantApesFile.substring(pathGlobal.length()+1);
-					
-						//Ajout dans le DependenciesParser du Pog correspondant
-						//addPog(Apes,Pog)
-						DP.addPog(pathSemiGlobal,pathToRoot);
-					}
-					else
-					{
-						DP.addPog(pathToRoot);
-					}
-					//TODO
-					//Appelle a la methode qui permet de calculer le chemin relatif
-					//relative(Pog,Apes)
-					//String relativeToApes = relative(node.getLocalPath(),pathDependantApesFile);
-					
-					//Modification du fichier POG	
-					//POGP.setApesPathToRelative(relativeToApes);
-					
-					
-					
-					
-					
-				}
-					
-					
-				//Si le fichier est anciens
-				else if (SvnHistory.isModified(toCommit))
-				{
 				
-					//On essaie de supprimer le fichier de la liste des fichiers ? modifier
-					DP.delToUpdate(pathToRoot);
-					
-										
-				}
-				//Cas d'erreur
-				//Le fichier n'est pas nouveau, et il n'a pas ?t? modifi?
-				else
-				{
-					
-				}
+				
 						
 			}
+			
+//			-----------------------------------------
+			//Si le fichier est un iepp -->
+			//-----------------------------------------
+			else if (nameOfFile.endsWith(".iepp")) 
+			{
+				sendIeppFile(toCommit,node,pathToRoot);
+			}
+			//Cas d'erreur
 			else
 			{
 				
@@ -186,7 +87,132 @@ public class CCommit{
 		
 	}
 	
+	/*
+	 * Permet de rajouter un fichier Iepp au fichier de dependances
+	 * 
+	 * 
+	 */
+	protected void sendIeppFile(File toCommit, CTreeNode node, String pathToRoot) {
 	
+		
+		DependenciesParser DP = DependenciesParser.getInstance();
+		
+			//Modifier l'interieur du fichier pog en
+			//mettant en relatif le chemin du fichier apes dependant
+			
+			//Ouverture du parser du fichier POG Correspondant
+			IeppNitParser INP = new IeppNitParser(toCommit);	
+			//Recuperation du chemin absolue du fichier apes d?pendant
+			ArrayList pathDependantApesFile = INP.getApes();
+			
+			
+			
+			
+		
+			
+	
+		
+	}
+
+
+
+	protected void sendPogFile(File toCommit, CTreeNode node, String pathToRoot) {
+
+		
+		DependenciesParser DP = DependenciesParser.getInstance();
+		
+		
+		//	Si le fichier est tout neuf
+		if (!SvnHistory.isUnderVersion(toCommit))
+		{
+		
+			//Modifier l'interieur du fichier pog en
+			//mettant en relatif le chemin du fichier apes dependant
+			
+			//Ouverture du parser du fichier POG Correspondant
+			POGParser POGP = new POGParser(toCommit);	
+			//Recuperation du chemin absolue du fichier apes d?pendant
+			String pathDependantApesFile = POGP.getPathApesFile();
+			
+			if (!pathDependantApesFile.equals(""))
+			{
+				//Recuperation du path absolue de la racine des projets
+				String pathGlobal = ((CTreeNode)((IProjectTree)XAGDOP.getInstance().getTree()).getModel().getRoot()).getLocalPath();
+				//Recuperation du pathToRoot du fichier Apes en 
+				//Enlevant les premiers caracteres correspondant au debut du chemin absolue
+				String pathSemiGlobal = pathDependantApesFile.substring(pathGlobal.length()+1);
+			
+				//Ajout dans le DependenciesParser du Pog correspondant
+				//addPog(Apes,Pog)
+				DP.addPog(pathSemiGlobal,pathToRoot);
+			}
+			else
+			{
+				DP.addPog(pathToRoot);
+			}
+			//TODO
+			//Appelle a la methode qui permet de calculer le chemin relatif
+			//relative(Pog,Apes)
+			//String relativeToApes = relative(node.getLocalPath(),pathDependantApesFile);
+			
+			//Modification du fichier POG	
+			//POGP.setApesPathToRelative(relativeToApes);
+			
+			
+			
+			
+			
+		}
+			
+			
+		//Si le fichier est anciens
+		else if (SvnHistory.isModified(toCommit))
+		{
+			//Indiquer qu'il faut modifier les fichiers Iepp dependant
+
+			//On recupere la liste de tous les iepp dependant de l'apes
+			
+			ArrayList allIepp = DP.getIeppFromPog(pathToRoot);
+			
+			
+			
+			//Initialisation du parcours
+			int j = 0;
+
+			
+
+			//On parcours la liste
+			for (j=0;j<allIepp.size();j++)
+			{
+				//On indique qu'il faut mettre a jour tous les fichiers
+				//Qui sont d?pendants du fichier apes que l'on veux envoyer
+				//A condition qu'il ne soit pas deja a modifier
+				if (!DP.isToUpdate((String)allIepp.get(j)))
+				{
+					//Ajouter dans la liste
+					DP.addToUpdate((String)allIepp.get(j));
+				}
+			}
+			
+			
+			
+			//On essaie de supprimer le fichier de la liste des fichiers ? modifier
+			DP.delToUpdate(pathToRoot);
+			
+								
+		}
+		//Cas d'erreur
+		//Le fichier n'est pas nouveau, et il n'a pas ?t? modifi?
+		else
+		{
+			
+		}
+		
+		
+	}
+
+
+
 	public void recCommit(CTreeNode node,String extention)
 	{
 		//On regarde si le node est un fils
@@ -215,6 +241,99 @@ public class CCommit{
 		
 	}
 	
+	protected void sendApesFile(File toCommit,CTreeNode node, String pathToRoot)
+	{
+		
+		DependenciesParser DP = DependenciesParser.getInstance();
+		//Si le fichier est tout neuf
+		if (!SvnHistory.isUnderVersion(toCommit))
+		{
+			//Pas d'entrainement de verification de fichier pog, car fichier tout nouveau
+
+			//Rajout dans le fichier de d?pendances le fichier apes que l'on veut envoyer
+			
+			DP.addApes(pathToRoot);
+			
+			
+			
+								
+		}
+//		Si le fichier est ancien
+		else if (node.isModified())
+		{
+
+			//On vient de modifier le fichier apes
+			//Il faut donc dire de changer les pog dependants
+			
+			
+			//On recupere la liste de tous les pogs dependant de l'apes
+			
+			ArrayList allPog = DP.getPogFromApes(pathToRoot);
+			
+			
+			
+			//Initialisation du parcours
+			int i = 0;
+
+			
+
+			//On parcours la liste
+			for (i=0;i<allPog.size();i++)
+			{
+				//On indique qu'il faut mettre a jour tous les fichiers
+				//Qui sont d?pendants du fichier apes que l'on veux envoyer
+				//A condition qu'il ne soit pas deja a modifier
+				if (!DP.isToUpdate((String)allPog.get(i)))
+				{
+					//Ajouter dans la liste
+					DP.addToUpdate((String)allPog.get(i));
+				}
+			}
+			
+			//On vient de modifier le fichier apes
+			//Il faut donc dire de changer les iepp dependants
+			
+			
+			//On recupere la liste de tous les iepp dependant de l'apes
+			
+			ArrayList allIepp = DP.getIeppFromApes(pathToRoot);
+			
+			
+			
+			//Initialisation du parcours
+			int j = 0;
+
+			
+
+			//On parcours la liste
+			for (j=0;j<allIepp.size();j++)
+			{
+				//On indique qu'il faut mettre a jour tous les fichiers
+				//Qui sont d?pendants du fichier apes que l'on veux envoyer
+				//A condition qu'il ne soit pas deja a modifier
+				if (!DP.isToUpdate((String)allIepp.get(j)))
+				{
+					//Ajouter dans la liste
+					DP.addToUpdate((String)allIepp.get(j));
+				}
+			}
+			
+			
+			
+			
+			
+			
+			
+
+		}
+		
+			//Cas d'erreur
+		else
+		{
+		
+		}
+		
+	}
 	
 	public void commitFile(CTreeNode currentNode,String comment){
 		
