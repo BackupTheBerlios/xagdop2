9f1dbefde50ba6842bcf3f90dba0f559a87acc26
elween <elween>
2006-02-10 08:16:45 +0000

    Modification pour le fonctionnement du test sur la m?thode Affecter

--- XAGDOP/xagdop/JUnit/CAffectTest.java
+++ XAGDOP/xagdop/JUnit/CAffectTest.java
@@ -7,26 +7,27 @@ import xagdop.Controleur.CAffect;
 import xagdop.Model.User;
 import xagdop.Parser.ProjectsParser;
 import xagdop.Parser.UsersParser;
-import xagdop.Util.ErrorManager;
+
 
 public class CAffectTest extends TestCase {
 
 	/*
 	 * Test method for 'xagdop.Controleur.CAffect.affecter(String, String, boolean, boolean, boolean, boolean)'
 	 */
-	public void testAffecter() {
+	public void testAffecter()  throws NullPointerException, Exception{
 		CAffect CA = new CAffect();
 		
 		//Creer un nouveau utilisateur
 		User us = new User("toto","totopass",true,false);
 		UsersParser usrp=null;
-		try {
+		
 			usrp = UsersParser.getInstance();
 			usrp.addUser(us);
 			
+			
+			
 			//Creer un nouveau projet
 			ProjectsParser pp = ProjectsParser.getInstance();
-			pp.addProject("projTest",us,"projDescr");
 			
 			//Tests
 			CA.affecter("projTest",us.getLogin(),true,false,true,false);
@@ -41,9 +42,7 @@ public class CAffectTest extends TestCase {
 			
 			//Supprimer le projet
 			pp.removeProject("projTest");
-		} catch (Exception e1) {
-			ErrorManager.getInstance().display();
-		}
+			
 		   
 	}
 
