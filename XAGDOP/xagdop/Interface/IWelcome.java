67f7e1c0c63bd9b10e7befdd9358d980a099c24d
kekile <kekile>
2006-02-16 21:45:02 +0000

    Correction des warnings

--- XAGDOP/xagdop/Interface/IWelcome.java
+++ XAGDOP/xagdop/Interface/IWelcome.java
@@ -7,68 +7,55 @@
 package xagdop.Interface;
 
 import java.awt.BorderLayout;
-import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JTextArea;
-import javax.swing.JTextPane;
-import javax.swing.WindowConstants;
 
 /**
  *
  * @author  Drez
  */
 public class IWelcome extends JFrame {
-    
-    /**
-	 * 
-	 */
+
 	private static final long serialVersionUID = 7319233017920832803L;
-	 // Variables declaration
+	/*Variables declaration*/
     private  JButton buttonNext;
-   
     private  JLabel logoLabel;
     private  JPanel panel;
-    
     private	 JLabel welcomeText;
 
-    // End of variables declaration
-	
 	/** Creates new form Acceuil */
-	
-	
-	
 	
     public IWelcome() {
         initComponents();
     }
     
     private void initComponents() {
-        GridBagConstraints gridBagConstraints;
+        /*Construction du panel */
+    	GridBagConstraints gridBagConstraints;
         gridBagConstraints = new GridBagConstraints();
         panel = new  JPanel();
         panel.setLayout(new GridBagLayout());
         
         /*Le texte de bienvenue */
         welcomeText = new JLabel();
-        welcomeText.setText("<html>Bienvenue dans XAGDOP. Vous devez configurer l'application pour pouvoir vous en servir. <br> Veuillez cliquer sur suivant pour acceder a la fenetre de configuration de l'application</html>");
+        welcomeText.setText("<html>Bienvenue dans XAGDOP.<br> Vous devez configurer l'application pour pouvoir vous en servir. <br> Veuillez cliquer sur suivant pour acceder a la fenetre de configuration de l'application</html>");
         gridBagConstraints.gridx = 0;
         gridBagConstraints.gridy = 0;
         gridBagConstraints.gridwidth = 2;
         gridBagConstraints.gridheight =1 ;
         gridBagConstraints.insets= new Insets(15,10,0,10);
+        gridBagConstraints.anchor =  GridBagConstraints.CENTER;
         panel.add(welcomeText, gridBagConstraints);
 
-        /*Le bouton suivant*/
+        /*Le bouton suivant */
         buttonNext = new  JButton();
         buttonNext.setText("Suivant");
         gridBagConstraints.gridx = 1;
@@ -78,14 +65,14 @@ public class IWelcome extends JFrame {
         gridBagConstraints.insets= new Insets(15,0,10,10);
         gridBagConstraints.anchor =  GridBagConstraints.SOUTHEAST;
         panel.add(buttonNext, gridBagConstraints);
-       
+       /*Action associée au bouton */
         buttonNext.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent evt) {
                 buttonNextActionPerformed(evt);
             }
         });
        
-        /*Le logo*/
+        /*Le logo */
         logoLabel = new  JLabel(new ImageIcon(XAGDOP.class.getResource("/xagdop/ressources/Icon/LogoXAGDOP5.jpg")));
         gridBagConstraints.gridx = 0;
         gridBagConstraints.gridy = 1;
@@ -95,12 +82,10 @@ public class IWelcome extends JFrame {
         gridBagConstraints.fill = GridBagConstraints.VERTICAL;
         panel.add(logoLabel, gridBagConstraints);
         
+        /*Creation de la fenetre */
         getContentPane().add(panel,  BorderLayout.CENTER);
-
         pack();
-        
-        setResizable(true) ;
-        
+        setResizable(false) ;
     }
     
     private void buttonNextActionPerformed(ActionEvent evt) {
@@ -108,6 +93,4 @@ public class IWelcome extends JFrame {
     			iclp.setVisible(true);
     			this.setVisible(false);
     	    }
-
-    
 }
