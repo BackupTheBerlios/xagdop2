77b97296401543928cde90838a2d1255040bd86f
jeremy <jeremy>
2006-02-16 19:22:53 +0000

    Le thread commit est la !

---
+++ XAGDOP/xagdop/Thread/ThreadCommit.java
@@ -0,0 +1,42 @@
+package xagdop.Thread;
+
+import java.util.ArrayList;
+import org.tmatesoft.svn.core.SVNException;
+import xagdop.Controleur.CTreeNode;
+import xagdop.Interface.XAGDOP;
+import xagdop.Svn.SvnCommit;
+import xagdop.Util.ErrorManager;
+
+
+
+
+public class ThreadCommit extends Thread {
+
+	CTreeNode node;
+	ThreadWait tWait;
+	ArrayList list;
+	String comment;
+	public ThreadCommit(CTreeNode treeNode,String comment,ThreadWait tWait){	
+		this.node = treeNode ;
+		this.comment = comment ;
+		this.tWait = tWait;
+	}
+	
+	public void run() {
+		
+		
+		try {
+			SvnCommit svnC = new SvnCommit();
+			svnC.commit(this.node,comment);
+		} catch (SVNException e) {
+			ErrorManager.getInstance().display();
+		}catch (Exception e){
+			ErrorManager.getInstance().display();
+		}finally{
+			tWait.arreter();			
+		}
+		
+	}
+	
+	
+}
