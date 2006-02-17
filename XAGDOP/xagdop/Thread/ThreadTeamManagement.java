2cf20d10160186bdafeca8cc3d35cc8ff15435c8
nephos <nephos>
2006-02-17 15:22:22 +0000

    Ajout du refresh

--- XAGDOP/xagdop/Thread/ThreadTeamManagement.java
+++ XAGDOP/xagdop/Thread/ThreadTeamManagement.java
@@ -2,7 +2,11 @@ package xagdop.Thread;
 
 import javax.swing.JTable;
 
+import xagdop.Controleur.CRole;
 import xagdop.Controleur.CTeamManagement;
+import xagdop.Controleur.CTree;
+import xagdop.Interface.IProjectTree;
+import xagdop.Interface.XAGDOP;
 import xagdop.Util.ErrorManager;
 
 
@@ -31,7 +35,9 @@ public class ThreadTeamManagement extends Thread {
 				CTeamM.Apply((String)JT.getValueAt(i,0),((Boolean)JT.getValueAt(i,1)).booleanValue(),((Boolean)JT.getValueAt(i,2)).booleanValue(),((Boolean)JT.getValueAt(i,3)).booleanValue(),((Boolean)JT.getValueAt(i,4)).booleanValue());
 			
 			i++;  
-		}        					
+		}        
+			CRole.getInstance().refreshRole();
+			((CTree)((IProjectTree)XAGDOP.getInstance().getTree()).getModel()).refreshFirst(((IProjectTree)XAGDOP.getInstance().getTree()).getCurrentProject());
 		} catch (Exception e) {
 			ErrorManager.getInstance().display();
 		}
