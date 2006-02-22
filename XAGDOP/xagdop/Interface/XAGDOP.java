3bb2b6df784c17303b56d54aec6edd55a254f565
kekile <kekile>
2006-02-22 10:53:22 +0000

    changement icon

--- XAGDOP/xagdop/Interface/XAGDOP.java
+++ XAGDOP/xagdop/Interface/XAGDOP.java
@@ -92,7 +92,7 @@ public class XAGDOP extends JFrame{
 	
 	protected JMenu menuEdite = new JMenu(Bundle.getText("main.menu.edit"));
 	protected JMenuItem menuEditeCommit = new JMenuItem(Bundle.getText("main.menu.edite.commit"), new ImageIcon(XAGDOP.class.getResource("/xagdop/ressources/Icon/envoyer.jpg")));
-	protected JMenuItem menuEditeUpdate = new JMenuItem(Bundle.getText("main.menu.edite.update"), new ImageIcon(XAGDOP.class.getResource("/xagdop/ressources/Icon/reload.png")));
+	protected JMenuItem menuEditeUpdate = new JMenuItem(Bundle.getText("main.menu.edite.update"), new ImageIcon(XAGDOP.class.getResource("/xagdop/ressources/Icon/update.jpg")));
 	protected JMenuItem menuEditeCheck = new JMenuItem(Bundle.getText("main.menu.edite.checkout"));
 	
 	protected JMenu menuShow = new JMenu(Bundle.getText("main.menu.show"));
@@ -141,7 +141,7 @@ public class XAGDOP extends JFrame{
 		projet = new JButton(new ImageIcon(imageURL));
 		imageURL = XAGDOP.class.getResource("/xagdop/ressources/Icon/delete.jpg");
 		delProject = new JButton(new ImageIcon(imageURL));
-		imageURL = XAGDOP.class.getResource("/xagdop/ressources/Icon/reload.png");
+		imageURL = XAGDOP.class.getResource("/xagdop/ressources/Icon/update11.jpg");
 		update = new JButton(new ImageIcon(imageURL));		
 		imageURL = XAGDOP.class.getResource("/xagdop/ressources/Icon/configure.png");
 		preferences = new JButton(new ImageIcon(imageURL));		
