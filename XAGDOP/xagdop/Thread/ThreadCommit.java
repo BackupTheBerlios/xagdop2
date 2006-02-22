7d7e672ece79cce44b415f553e3ab58c34b05a12
jeremy <jeremy>
2006-02-22 12:31:59 +0000

    Changement du thread commit, appel a l'instance de IWaiting

--- XAGDOP/xagdop/Thread/ThreadCommit.java
+++ XAGDOP/xagdop/Thread/ThreadCommit.java
@@ -1,48 +1,57 @@
 package xagdop.Thread;
 
 import java.util.ArrayList;
-
+import javax.xml.xpath.XPathExpressionException;
 import org.tmatesoft.svn.core.SVNException;
-
+import xagdop.Controleur.CCommit;
 import xagdop.Controleur.CTree;
 import xagdop.Controleur.CTreeNode;
 import xagdop.Interface.IProjectTree;
+import xagdop.Interface.IWaiting;
 import xagdop.Interface.XAGDOP;
-import xagdop.Svn.SvnCommit;
+import xagdop.Parser.DependenciesParser;
 import xagdop.Util.ErrorManager;
 
 
 
 
 public class ThreadCommit extends Thread {
-
+	
 	CTreeNode node;
-	ThreadWait tWait;
+	
 	ArrayList list;
 	String comment;
-	public ThreadCommit(CTreeNode treeNode,String comment,ThreadWait tWait){	
+	public ThreadCommit(CTreeNode treeNode,String comment){	
 		this.node = treeNode ;
 		this.comment = comment ;
-		this.tWait = tWait;
+		
 	}
 	
 	public void run() {
 		
-		
+		CCommit CC = null;
 		try {
-			SvnCommit svnC = new SvnCommit();
-			svnC.commit(this.node,comment);
-		} catch (SVNException e) {
+			CC = new CCommit(this.node);
+			CC.DependancesSendInitialize(this.node);
+			CC.commitFile(this.node,this.comment);
+			DependenciesParser dp = DependenciesParser.getInstance();
+			dp.publish(dp.getFile(this.node.getProject().getName()));
+		} catch (SVNException e1) {
 			ErrorManager.getInstance().display();
-		}catch (Exception e){
+		} catch (XPathExpressionException e2) {
 			ErrorManager.getInstance().display();
+		} catch (Exception e1) {
+			// TODO Auto-generated catch block
+			ErrorManager.getInstance().display();
+			
 		}finally{
 			try {
 				((CTree)((IProjectTree)XAGDOP.getInstance().getTree()).getModel()).refreshFirst(node.getProject());
 			} catch (SVNException e) {
-				System.out.println("popopopo");
+			
 			}
-			tWait.arreter();			
+		
+			IWaiting.getInstance().arreter();			
 		}
 		
 	}
