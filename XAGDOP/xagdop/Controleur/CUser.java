212aad9c011ce3692be307e1fc4a528049e526e7
kekile <kekile>
2006-02-28 22:21:10 +0000

    On ne peut plus creer d'utilisateur ayant un identifiant vide, cela leve une Exception

--- XAGDOP/xagdop/Controleur/CUser.java
+++ XAGDOP/xagdop/Controleur/CUser.java
@@ -24,9 +24,11 @@ public class CUser {
 	public boolean verifUser(String login, String passwd) throws Exception{
 		UsersParser uParser = UsersParser.getInstance();
 		SvnConnect.getInstance().setName(login);
+		
 		/*On recupere l'utilisateur qui possède le login et le mot de passe inscrit */
 		User user = uParser.getUser(login,CEncrypt.getEncodedPassword(passwd));
     	if (user != null){
+    		
     		/* Si l'utilisateur existe, on se connecte a l'application */
     		IPreferences.setDefaultPath(PreferenciesParser.getInstance().buildPreferencies().getLocal()+File.separator+user.getLogin()+File.separator);
     		XAGDOP.getInstance().setUser(user);
@@ -43,36 +45,46 @@ public class CUser {
 	{
 		UsersParser up = UsersParser.getInstance();
 		
-		/* On vérifie si les deux mots de passe sont identiques */
-		if (passwd.equals(passwdconf)){
+		/* On vérifie le champs est rempli*/
+		if (login.length()>0){
 		
-			/* On vérifie la longueur du mot de passe*/
-			if (passwd.length()>3){
+			/* On vérifie si les deux mots de passe sont identiques */
+			if (passwd.equals(passwdconf)){
+		
+				/* On vérifie la longueur du mot de passe*/
+				if (passwd.length()>3){
     			
-				/* On vérifie si le login est déjà attribué*/
-				if (!up.isUser(login)){
-    				up.addUser(login,CEncrypt.getEncodedPassword(passwd));
-    				JOptionPane.showMessageDialog(null ,Bundle.getText("cuser.create.text"), Bundle.getText("iusercreate.title"), 1) ;
-    			}
-    			else{
-    				/* On renvoi une exception car le login existe déja */
-    				ErrorManager.getInstance().setErrMsg(Bundle.getText("cuser.exist.text"));
-    				ErrorManager.getInstance().setErrTitle(Bundle.getText("iusercreate.title"));
-    				throw new Exception();
-    			}
-    		}
-    		else{
-    			/* On renvoi une exception car le mot de passe ne contient pas au minimum 4 caractères*/
-    			ErrorManager.getInstance().setErrMsg(Bundle.getText("cuser.length.text"));
-    			ErrorManager.getInstance().setErrTitle(Bundle.getText("iusercreate.title"));
-    			throw new Exception();    		
-    		}
+					/* On vérifie si le login est déjà attribué*/
+					if (!up.isUser(login)){
+						up.addUser(login,CEncrypt.getEncodedPassword(passwd));
+						JOptionPane.showMessageDialog(null ,Bundle.getText("cuser.create.text"), Bundle.getText("iusercreate.title"), 1) ;
+					}
+					else{
+						/* On renvoi une exception car le login existe déja */
+						ErrorManager.getInstance().setErrMsg(Bundle.getText("cuser.exist.text"));
+						ErrorManager.getInstance().setErrTitle(Bundle.getText("iusercreate.title"));
+						throw new Exception();
+					}
+				}
+				else{
+					/* On renvoi une exception car le mot de passe ne contient pas au minimum 4 caractères*/
+					ErrorManager.getInstance().setErrMsg(Bundle.getText("cuser.length.text"));
+					ErrorManager.getInstance().setErrTitle(Bundle.getText("iusercreate.title"));
+					throw new Exception();    		
+				}
+			}
+			else{
+				/* On renvoi une exception car la confirmation du mot de passe est différente du mot de passe*/
+				ErrorManager.getInstance().setErrMsg(Bundle.getText("cuser.notequals.text"));
+				ErrorManager.getInstance().setErrTitle(Bundle.getText("iusercreate.title"));
+				throw new Exception();   
+			}
 		}
 		else{
-			/* On renvoi une exception car la confirmation du mot de passe est différente du mot de passe*/
-			ErrorManager.getInstance().setErrMsg(Bundle.getText("cuser.notequals.text"));
+			/* On renvoi une exception car le login est vide*/
+			ErrorManager.getInstance().setErrMsg(Bundle.getText("cuser.loginlength.text"));
 			ErrorManager.getInstance().setErrTitle(Bundle.getText("iusercreate.title"));
-			throw new Exception();   
+			throw new Exception();    		
 		}
 	}
 }
