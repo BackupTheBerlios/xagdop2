d6a1860596eb5c5efb8265afe2dcdfc598088fb5
jeremy <jeremy>
2006-03-01 00:59:35 +0000

    mise en thread du changement de repesitory

--- XAGDOP/xagdop/Interface/Preferences/IPreferences.java
+++ XAGDOP/xagdop/Interface/Preferences/IPreferences.java
@@ -25,18 +25,15 @@ import javax.swing.event.TreeSelectionEvent;
 import javax.swing.event.TreeSelectionListener;
 import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.TreeSelectionModel;
-
 import org.tmatesoft.svn.core.SVNException;
-
 import xagdop.Controleur.CPreferencies;
 import xagdop.Controleur.CTree;
 import xagdop.Controleur.CTreeNode;
+import xagdop.Interface.IWaiting;
 import xagdop.Interface.XAGDOP;
-import xagdop.Parser.ProjectsParser;
-import xagdop.Parser.UsersParser;
 import xagdop.Svn.SvnConnect;
 import xagdop.Svn.SvnHistory;
-import xagdop.Svn.SvnUpdate;
+import xagdop.Thread.ThreadServerChanged;
 import xagdop.Util.ErrorManager;
 import xagdop.ressources.Bundle;
 
@@ -337,40 +334,12 @@ public class IPreferences extends JFrame implements TreeSelectionListener{
 					ErrorManager.getInstance().display();
 					break;
 				}
-				break;
-			case REMOTE_PATH_REF: 
-			//Refresh des fichiers de projet et de user
-			try{			
-				//On remet le nouveau path
-				SvnConnect.getInstance().setUrl(RemotePathPanel.getRemotePath());
-				//On s'y reconnecte
-				SvnConnect.getInstance().connect();
-				if(!SvnConnect.getInstance().getRepositoryUUID().equals(SvnHistory.getRepositoryUUID(new File(IPreferences.getDefaultPath())))){
-					ErrorManager.getInstance().setErrMsg("Le répertoire de travail choisi est déjà utilisé pour un autre dépôt SubVersion, en choisir un autre avant de changer l'adresse du serveur");
-					ErrorManager.getInstance().setErrTitle("Probleme serveur");
-					ErrorManager.getInstance().display();
-					remotePathPanel.setRemotePath(CPreferencies.getServerPath());
-					break;
-				}
-				//System.out.println(SvnConnect.getInstance().getRepositoryUUID());
-				//System.out.println(SvnHistory.getRepositoryUUID(new File(IPreferences.getDefaultPath())));
-				//On fait un update
-				SvnUpdate svnu = new SvnUpdate();
-				//On recupere les fichiers user et project
-				svnu.getFiles();
-				//On remet a jour les arbres en memoire
-				ProjectsParser.getInstance();
-				UsersParser.getInstance();
-				//On fait un refresh du local
-				XAGDOP.getInstance().refreshTree();
-				CPreferencies.setServerPath(RemotePathPanel.getRemotePath());
-			}catch (Exception e1)
-			{
-				ErrorManager.getInstance().display();
-				remotePathPanel.setRemotePath(CPreferencies.getServerPath());
-				break;
-			}
-			
+			case REMOTE_PATH_REF: CPreferencies.setServerPath(RemotePathPanel.getRemotePath());
+				IWaiting iWait = IWaiting.getInstance();
+				iWait.demarrer();
+				ThreadServerChanged tv = new ThreadServerChanged(remotePathPanel);
+				tv.start();
+
 			break;
 			case LNF_REF: CPreferencies.setDefaultLNF(LookAndFeelPanel.getLNF());
 			break;
