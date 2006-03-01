fec8f73a9445fcd2a51e3022c7fefc1e9cfca42e
jeremy <jeremy>
2006-03-01 00:27:16 +0000

    creation

---
+++ XAGDOP/xagdop/Thread/ThreadRemoveUser.java
@@ -0,0 +1,36 @@
+package xagdop.Thread;
+
+import xagdop.Interface.IWaiting;
+
+import xagdop.Parser.ProjectsParser;
+
+
+
+
+
+public class ThreadRemoveUser extends Thread {
+
+
+
+	ProjectsParser PP;
+	String user;
+	String projectName;
+	
+	public ThreadRemoveUser(ProjectsParser PP,String projectName, String user) {
+
+		this.PP = PP;
+		this.user = user;
+		this.projectName = projectName;
+	}
+
+	public void run() {
+			try {
+				PP.removeUser(this.projectName,this.user);
+			} catch (Exception e) {
+				// :p
+			}
+			IWaiting.getInstance().arreter();
+	}
+	
+	
+}
